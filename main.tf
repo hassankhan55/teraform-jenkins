16da7f3cf0c4365719292b4d99c3a76c88bf32ac
hassankhan55 <tenfold5455@gmail.com>
2022-04-07 12:43:41 -0400

    adding s3 resource

--- main.tf
+++ main.tf
@@ -1,9 +1,19 @@
-resource "random_string" "random" {
-  length           = 16
-  special          = true
-  override_special = "/@£$"
-}
+# resource "random_string" "random" {
+#   length           = 16
+#   special          = true
+#   override_special = "/@£$"
+# }
 
-output "random_name" {
-    value = random_string.random.id
+# output "random_name" {
+#     value = random_string.random.id
+# }
+
+
+resource "aws_s3_bucket" "b" {
+  bucket = "my-tf-hassan-bucket"
+
+  tags = {
+    Name        = "my-tf-hassan-bucket"
+    Environment = "Dev"
+  }
 }
